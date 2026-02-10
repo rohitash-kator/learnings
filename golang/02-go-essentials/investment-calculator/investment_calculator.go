6d2380243d3ffe865f02f52e91fa85a08739ecc9
Rohitash Kator <rohitash.kator@talentica.com>
2026-02-10 09:43:47 +0530

    Working with Functions: Refactor: Enhance investment and profit calculators with modular functions for user input and calculations

--- golang/02-go-essentials/investment-calculator/investment_calculator.go
+++ golang/02-go-essentials/investment-calculator/investment_calculator.go
@@ -5,25 +5,31 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 
 	var investmentAmount float64
 	var years float64
 
 	expectedReturnRate := 5.5
 
-	fmt.Print("Enter investment amount: ")
+	// fmt.Print("Enter investment amount: ")
+	outputText("Enter investment amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Enter expected return rate: ")
+	// fmt.Print("Enter expected return rate: ")
+	outputText("Enter expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Enter investment duration: ")
+	// fmt.Print("Enter investment duration: ")
+	outputText("Enter investment duration: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	// formattedFutureValue := fmt.Sprintf("Future Value: %.1f\n", futureRealValue)
 	// formattedFutureRealValue := fmt.Sprintf("Future Value (adjusted for inflation): %.1f\n", futureRealValue)
@@ -37,4 +43,16 @@ func main() {
 
 	// fmt.Println("Future Value (adjusted for inflation)", futureRealValue)
 	// fmt.Print(formattedFutureValue, formattedFutureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (futureValue float64, realFutureValue float64) {
+	futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	realFutureValue = futureValue / math.Pow(1+inflationRate/100, years)
+
+	return futureValue, realFutureValue
+	// return
 }
--- golang/02-go-essentials/profit-calculator/profit_calculator.go
+++ golang/02-go-essentials/profit-calculator/profit_calculator.go
@@ -3,24 +3,51 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	// var revenue float64
+	// var expenses float64
+	// var taxRate float64
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
+	// fmt.Print("Enter revenue: ")
+	// fmt.Scan(&revenue)
 
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
+	// fmt.Print("Enter expenses: ")
+	// fmt.Scan(&expenses)
 
-	fmt.Print("Enter tax rate: ")
-	fmt.Scan(&taxRate)
+	// fmt.Print("Enter tax rate: ")
+	// fmt.Scan(&taxRate)
 
+	revenue := getUserInput("Enter revenue: ")
+	expenses := getUserInput("Enter expenses: ")
+	taxRate := getUserInput("Enter tax rate: ")
+
+	// earningBeforeTax := revenue - expenses
+	// earningAfterTax := earningBeforeTax * (1 - taxRate/100)
+	// ratio := earningBeforeTax / earningAfterTax
+
+	earningBeforeTax, earningAfterTax, ratio := calculateFinancial(revenue, expenses, taxRate)
+
+	// fmt.Print("Earnings before tax: ", earningBeforeTax)
+	// fmt.Print("\nEarnings after tax (Profit): ", earningAfterTax)
+	// fmt.Print("\nEBT/profit ratio: ", ratio)
+
+	fmt.Printf("Earnings before tax: %.1f\n", earningBeforeTax)
+	fmt.Printf("Earnings after tax (Profit): %.1f\n", earningAfterTax)
+	fmt.Printf("EBT/profit ratio: %.1f\n", ratio)
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+
+	return userInput
+}
+
+func calculateFinancial(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	earningBeforeTax := revenue - expenses
 	earningAfterTax := earningBeforeTax * (1 - taxRate/100)
 	ratio := earningBeforeTax / earningAfterTax
 
-	fmt.Print("Earnings before tax: ", earningBeforeTax)
-	fmt.Print("\nEarnings after tax (Profit): ", earningAfterTax)
-	fmt.Print("\nEBT/profit ratio: ", ratio)
+	return earningBeforeTax, earningAfterTax, ratio
 }
