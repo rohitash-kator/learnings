115ff5fc0adffe606d7277028e81e5bdee86feb2
Rohitash Kator <rohitash.kator@talentica.com>
2026-02-10 21:23:17 +0530

    Structs: Initialize Go module and add user struct implementation: Create go.mod file and implement user struct with methods for user data handling and output.

---
+++ golang/05-struct-and-custom-types/go.mod
@@ -0,0 +1,3 @@
+module example.com/go-structs
+
+go 1.25.6
---
+++ golang/05-struct-and-custom-types/sturcts.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthDate := getUserData("Please enter your birth date (MM/DD/YYYY): ")
+
+	var appUser user
+
+	// Initialize the struct with no values (all fields will be set to their zero/nil values)
+	// appUser = user{}
+
+	// Initialize the struct with values for some fields and the rest will be set to their zero/nil values
+	// appUser = user{
+	// 	firstName: userFirstName,
+	// 	lastName:  userLastName,
+	// 	// birthDate: userBirthDate,
+	// 	createdAt: time.Now(),
+	// }
+
+	// Direct assignment of values to the struct fields using variable names and values (order is not important)
+	appUser = user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthDate: userBirthDate,
+		createdAt: time.Now(),
+	}
+
+	// Direct assignment of values to the struct fields using variable names and values (order is important)
+	// appUser = user{
+	// 	userFirstName,
+	// 	userLastName,
+	// 	userBirthDate,
+	// 	time.Now(),
+	// }
+
+	// outputUserDetails(&appUser)
+	appUser.outputUserDetails() // Call the method on the struct instance
+
+	appUser.clearUserName()
+
+	appUser.outputUserDetails()
+}
+
+// Using value receiver
+// func outputUserDetails(userInfo user) {
+// 	// ...
+// 	fmt.Println(userInfo.firstName, userInfo.lastName, userInfo.birthDate)
+// }
+
+// Using pointer receiver
+// func outputUserDetails(userInfo *user) {
+// 	// ...
+// 	// Dereference the pointer to get the value of the struct fields using the dot operator (dereferencing is optional)
+// 	fmt.Println((*userInfo).firstName, userInfo.lastName, userInfo.birthDate)
+// }
+
+// Attaching a method to a struct
+func (userInfo user) outputUserDetails() {
+	// ...
+	// Dereference the pointer to get the value of the struct fields using the dot operator (dereferencing is optional)
+	fmt.Println(userInfo.firstName, userInfo.lastName, userInfo.birthDate)
+}
+
+// Attaching a mutating method to a struct
+// Important: This method takes a pointer to the struct as the receiver so that the changes are reflected in the original struct instance,
+// otherwise the changes will be reflected in a copy of the struct instance and not the original struct instance
+func (userInfo *user) clearUserName() {
+	userInfo.firstName = ""
+	userInfo.lastName = ""
+
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+
+	fmt.Scan(&value)
+
+	return value
+}
