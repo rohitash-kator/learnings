0dfbce8937fdf37efd6ef82c536a143f8ec54e75
Rohitash Kator <rohitash.kator@talentica.com>
2026-02-10 22:31:34 +0530

    Structs: Refactor user struct usage by replacing direct initialization with method calls from the user package, enhancing code organization and readability.

--- golang/05-struct-and-custom-types/sturcts.go
+++ golang/05-struct-and-custom-types/sturcts.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
-)
 
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
+	"example.com/go-structs/user"
+)
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthDate := getUserData("Please enter your birth date (MM/DD/YYYY): ")
 
-	var appUser *user
+	var appUser *user.User
 
 	// Initialize the struct with no values (all fields will be set to their zero/nil values)
 	// appUser = user{}
@@ -32,7 +25,7 @@ func main() {
 	// }
 
 	// Direct assignment of values to the struct fields using variable names and values (order is not important)
-	appUser, err := newUser(userFirstName, userLastName, userBirthDate)
+	appUser, err := user.New(userFirstName, userLastName, userBirthDate)
 
 	if err != nil {
 		// panic(err)
@@ -49,56 +42,9 @@ func main() {
 	// }
 
 	// outputUserDetails(&appUser)
-	appUser.outputUserDetails() // Call the method on the struct instance
-
-	appUser.clearUserName()
-
-	appUser.outputUserDetails()
-}
-
-// Using value receiver
-// func outputUserDetails(userInfo user) {
-// 	// ...
-// 	fmt.Println(userInfo.firstName, userInfo.lastName, userInfo.birthDate)
-// }
-
-// Using pointer receiver
-// func outputUserDetails(userInfo *user) {
-// 	// ...
-// 	// Dereference the pointer to get the value of the struct fields using the dot operator (dereferencing is optional)
-// 	fmt.Println((*userInfo).firstName, userInfo.lastName, userInfo.birthDate)
-// }
-
-// Attaching a method to a struct
-func (userInfo user) outputUserDetails() {
-	// ...
-	// Dereference the pointer to get the value of the struct fields using the dot operator (dereferencing is optional)
-	fmt.Println(userInfo.firstName, userInfo.lastName, userInfo.birthDate)
-}
-
-// Attaching a mutating method to a struct
-// Important: This method takes a pointer to the struct as the receiver so that the changes are reflected in the original struct instance,
-// otherwise the changes will be reflected in a copy of the struct instance and not the original struct instance
-func (userInfo *user) clearUserName() {
-	userInfo.firstName = ""
-	userInfo.lastName = ""
-
-}
-
-// Factory/Constructor/Creation function to create a new user struct instance (Note: This is a regular function, not a method)
-// Important: This function returns a pointer to the user struct instance so that the changes are reflected in the original struct instance,
-// otherwise the changes will be reflected in a copy of the struct instance and not the original struct instance
-func newUser(firstName string, lastName string, birthDate string) (*user, error) {
-	if firstName == "" || lastName == "" || birthDate == "" {
-		return nil, errors.New("First name, Last name and birth date are required")
-	}
-
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthDate,
-		createdAt: time.Now(),
-	}, nil
+	appUser.OutputUserDetails() // Call the method on the struct instance
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
---
+++ golang/05-struct-and-custom-types/user/user.go
@@ -0,0 +1,59 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+// Using value receiver
+// func outputUserDetails(userInfo user) {
+// 	// ...
+// 	fmt.Println(userInfo.firstName, userInfo.lastName, userInfo.birthDate)
+// }
+
+// Using pointer receiver
+// func outputUserDetails(userInfo *user) {
+// 	// ...
+// 	// Dereference the pointer to get the value of the struct fields using the dot operator (dereferencing is optional)
+// 	fmt.Println((*userInfo).firstName, userInfo.lastName, userInfo.birthDate)
+// }
+
+// Attaching a method to a struct
+func (userInfo *User) OutputUserDetails() {
+	// ...
+	// Dereference the pointer to get the value of the struct fields using the dot operator (dereferencing is optional)
+	fmt.Println(userInfo.firstName, userInfo.lastName, userInfo.birthDate)
+}
+
+// Attaching a mutating method to a struct
+// Important: This method takes a pointer to the struct as the receiver so that the changes are reflected in the original struct instance,
+// otherwise the changes will be reflected in a copy of the struct instance and not the original struct instance
+func (userInfo *User) ClearUserName() {
+	userInfo.firstName = ""
+	userInfo.lastName = ""
+
+}
+
+// Factory/Constructor/Creation function to create a new user struct instance (Note: This is a regular function, not a method)
+// Important: This function returns a pointer to the user struct instance so that the changes are reflected in the original struct instance,
+// otherwise the changes will be reflected in a copy of the struct instance and not the original struct instance
+func New(firstName string, lastName string, birthDate string) (*User, error) {
+	if firstName == "" || lastName == "" || birthDate == "" {
+		return nil, errors.New("First name, Last name and birth date are required")
+	}
+
+	return &User{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+	}, nil
+}
