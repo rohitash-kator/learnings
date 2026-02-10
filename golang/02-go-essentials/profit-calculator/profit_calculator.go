01d86bdcdacbf9f60c35ac9da4fcf141b90aa20b
Rohitash Kator <rohitash.kator@talentica.com>
2026-02-10 11:13:14 +0530

    Exercise: Control stateemnts, functions and file handling with errors

--- golang/02-go-essentials/profit-calculator/profit_calculator.go
+++ golang/02-go-essentials/profit-calculator/profit_calculator.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
 	// var revenue float64
@@ -16,9 +20,32 @@ func main() {
 	// fmt.Print("Enter tax rate: ")
 	// fmt.Scan(&taxRate)
 
-	revenue := getUserInput("Enter revenue: ")
-	expenses := getUserInput("Enter expenses: ")
-	taxRate := getUserInput("Enter tax rate: ")
+	revenue, revenueErr := getUserInput("Enter revenue: ")
+
+	if revenueErr != nil {
+		fmt.Println(revenueErr)
+		return
+		// panic(revenueErr)
+	}
+
+	expenses, expensesErr := getUserInput("Enter expenses: ")
+
+	if expensesErr != nil {
+		fmt.Println(expensesErr)
+		return
+	}
+
+	taxRate, taxRateErr := getUserInput("Enter tax rate: ")
+
+	if taxRateErr != nil {
+		fmt.Println(taxRateErr)
+		return
+	}
+
+	// if revenueErr != nil || expensesErr != nil || taxRateErr != nil {
+	// 	fmt.Println()
+	// 	return
+	// }
 
 	// earningBeforeTax := revenue - expenses
 	// earningAfterTax := earningBeforeTax * (1 - taxRate/100)
@@ -33,15 +60,25 @@ func main() {
 	fmt.Printf("Earnings before tax: %.1f\n", earningBeforeTax)
 	fmt.Printf("Earnings after tax (Profit): %.1f\n", earningAfterTax)
 	fmt.Printf("EBT/profit ratio: %.1f\n", ratio)
+	storeResults(earningBeforeTax, earningBeforeTax, ratio)
 }
 
-func getUserInput(infoText string) float64 {
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
+
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("Value must be a positive number")
+	}
+
+	return userInput, nil
 }
 
 func calculateFinancial(revenue, expenses, taxRate float64) (float64, float64, float64) {
