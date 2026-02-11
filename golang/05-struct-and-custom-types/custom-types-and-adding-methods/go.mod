6134fa5b3cf8d04ffb6068858e7d74d0c8d970d5
Rohitash Kator <rohitash.kator@talentica.com>
2026-02-11 10:50:21 +0530

    Structs: Add custom types and methods implementation, including a note management system with file saving functionality and user input handling.

---
+++ golang/05-struct-and-custom-types/custom-types-and-adding-methods/go.mod
@@ -0,0 +1,3 @@
+module example.com/go-custom-types
+
+go 1.25.6
---
+++ golang/05-struct-and-custom-types/practice-structs/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
+)
+
+func main() {
+	title, content := getNoteData()
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+
+	err = userNote.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return
+	}
+
+	fmt.Println("Saving the note succeed!")
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note Title:")
+	content := getUserInput("Note Content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
