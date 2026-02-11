eb872d8ae3ac0b48e4c2f60ff9e872793c85a9a4
Rohitash Kator <rohitash.kator@talentica.com>
2026-02-11 13:00:18 +0530

    Interfaces: Generic functions. Implement generic and non-generic addition functions: Add a generic function to handle addition of various types and update main function to demonstrate usage.

--- golang/06-interfaces-and-generic-code/generic-code/main.go
+++ golang/06-interfaces-and-generic-code/generic-code/main.go
@@ -1,7 +1,19 @@
 package main
 
-func main() {}
+import "fmt"
 
+func main() {
+	result := add(1, 2)
+
+	fmt.Println(result)
+}
+
+// Generic function to add two values of any type
+func addGeneric[T int | float64 | string](a, b T) T {
+	return a + b
+}
+
+// Non-generic function to add two values of any type
 func add(a, b interface{}) interface{} {
 	aInt, aIsInt := a.(int)
 	bInt, bIsInt := b.(int)
