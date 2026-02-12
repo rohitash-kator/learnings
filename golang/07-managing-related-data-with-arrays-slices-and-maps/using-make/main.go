9f5376089f926e322c4edf99cc592bfbd1869abd
Rohitash Kator <rohitash.kator@talentica.com>
2026-02-12 15:20:49 +0530

    Arrays, Slices and Maps: Enhance using-make/main.go: Introduce a custom type for float maps, add methods for outputting map data, and demonstrate iteration over slices and maps. Update example usernames to prevent runtime errors and improve clarity in data structure usage.

--- golang/07-managing-related-data-with-arrays-slices-and-maps/using-make/main.go
+++ golang/07-managing-related-data-with-arrays-slices-and-maps/using-make/main.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+// Custom type to create a map[string]float64
 type floatMap map[string]float64
 
+// Method to output the map
 func (m floatMap) output() {
 	fmt.Println(m)
 }
@@ -12,7 +14,8 @@ func main() {
 	usernames := make([]string, 2, 5)
 
 	// usernames := []string{}
-	// usernames[0] = "rohitash-kator" // This will cause a runtime error of index out of range as the array is empty in case of declared array without using make
+
+	usernames[0] = "intact.infinity" // This will cause a runtime error of index out of range as the array is empty in case of declared array without using make
 
 	usernames = append(usernames, "rohitash_kator") // Add a new username to the slice
 	usernames = append(usernames, "itsrkator")      // Add a new username to the slice
@@ -21,6 +24,7 @@ func main() {
 
 	fmt.Println(usernames) // [rohitash_kator itsrkator]
 
+	// Using a built-in type map[string]float64
 	courseRatings := make(map[string]float64, 3)
 
 	courseRatings["go"] = 4.7
@@ -30,7 +34,9 @@ func main() {
 
 	fmt.Println(courseRatings)
 
+	// Using a custom type map[string]float64
 	ratings := make(floatMap, 3)
+
 	ratings["go"] = 4.7
 	ratings["react"] = 4.8
 	ratings["angular"] = 4.9
@@ -38,4 +44,13 @@ func main() {
 
 	ratings.output()
 
+	// Using a range to iterate over the slice
+	for index, username := range usernames {
+		fmt.Println("Index:", index, "Value:", username)
+	}
+
+	// Using a range to iterate over the map
+	for key, value := range ratings {
+		fmt.Println("Key:", key, "Value:", value)
+	}
 }
